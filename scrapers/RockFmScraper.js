c8f33f179a74892c2fb55ccd540f5c0d4a37611c
bonnici <nickmarsh@gmail.com>
2015-01-10 15:28:51 +1000

    Updated RockFM scraper to work with their new playlist

--- scrapers/RockFmScraper.js
+++ scrapers/RockFmScraper.js
@@ -5,7 +5,8 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var scrap = require("./CheerioScraper");
+//import scrap = require("./CheerioScraper");
+var jsonScrap = require("./JsonScraper");
 
 var winston = require("winston");
 
@@ -13,32 +14,57 @@ var RockFmScraper = (function (_super) {
     __extends(RockFmScraper, _super);
     function RockFmScraper(name) {
         _super.call(this, name);
-        this.url = "http://www.rockfm.co.uk";
     }
-    RockFmScraper.prototype.getUrl = function () {
-        return this.url;
+    RockFmScraper.prototype.getUrl = function (scraperParam) {
+        // This approximately the time that's used on the site, it returns with long polling for times after this
+        var timeToUse = (new Date().getTime() - (60 * 60 * 60)) * 10000;
+        return "http://ps9.pubnub.com/subscribe/sub-eff4f180-d0c2-11e1-bee3-1b5222fb6268/np_36/0/" + timeToUse + "?uuid=84d3e4f8-33d2-4a4c-a974-9d88a3f92965";
     };
 
-    RockFmScraper.prototype.parseCheerio = function ($, callback) {
-        var nowPlayingSpans = $('#now-playing-track span');
+    RockFmScraper.prototype.extractNowPlayingSong = function (jsonData) {
+        var artist = jsonData[0][0].artist.trim();
+        var track = jsonData[0][0].title.trim();
 
-        if (nowPlayingSpans.length < 3) {
-            winston.warn("RockFmScraper: Not enough now playing spans (" + nowPlayingSpans.length + ")");
-            callback(null, { Artist: null, Track: null });
-            return;
-        }
-
-        var artist = nowPlayingSpans.eq(1).text();
-        var song = nowPlayingSpans.eq(2).text();
-
-        if (!artist || !song) {
-            winston.warn("RockFmScraper: Invalid artist or song (" + artist + "/" + song + ")");
-            callback(null, { Artist: null, Track: null });
+        if (artist && track && artist != 'No Artist Info Available' && track != 'No Track info Available') {
+            return {
+                Artist: jsonData[0][0].artist.trim(),
+                Track: jsonData[0][0].title.trim()
+            };
         } else {
-            callback(null, { Artist: artist, Track: song });
+            winston.info("RockFmScraper: No song playing");
+            return { Artist: null, Track: null };
         }
     };
     return RockFmScraper;
-})(scrap.CheerioScraper);
+})(jsonScrap.JsonScraper);
 exports.RockFmScraper = RockFmScraper;
+/*
+export class RockFmScraper extends scrap.CheerioScraper {
+private url: string;
+constructor(name:string) {
+super(name);
+this.url = "http://www.rockfm.co.uk";
+}
+public getUrl(): string {
+return this.url;
+}
+public parseCheerio($:any, callback: (err, newNowPlayingSong: song.Song, justScrobbledSong?:song.Song) => void): void {
+var nowPlayingSpans = $('#now-playing-track span');
+if (nowPlayingSpans.length < 3) {
+winston.warn("RockFmScraper: Not enough now playing spans (" + nowPlayingSpans.length + ")");
+callback(null, { Artist: null, Track: null });
+return;
+}
+var artist = nowPlayingSpans.eq(1).text();
+var song = nowPlayingSpans.eq(2).text();
+if (!artist || !song) {
+winston.warn("RockFmScraper: Invalid artist or song (" + artist + "/" + song + ")");
+callback(null, { Artist: null, Track: null });
+}
+else {
+callback(null, { Artist: artist, Track: song });
+}
+}
+}
+*/
 //# sourceMappingURL=RockFmScraper.js.map
--- scrapers/RockFmScraper.ts
+++ scrapers/RockFmScraper.ts
@@ -1,16 +1,47 @@
 /// <reference path="../definitions/typescript-node-definitions/winston.d.ts"/>
 
-import scrap = require("./CheerioScraper");
+//import scrap = require("./CheerioScraper");
+import jsonScrap = require("./JsonScraper");
 import song = require("../Song");
 
 import winston = require("winston");
 
+export class RockFmScraper extends jsonScrap.JsonScraper {
+
+    constructor(name:string) {
+        super(name);
+    }
+
+    getUrl(scraperParam?:string): string {
+        // This approximately the time that's used on the site, it returns with long polling for times after this
+        var timeToUse = (new Date().getTime() - (60*60*60)) * 10000;
+        return "http://ps9.pubnub.com/subscribe/sub-eff4f180-d0c2-11e1-bee3-1b5222fb6268/np_36/0/" +
+            timeToUse + "?uuid=84d3e4f8-33d2-4a4c-a974-9d88a3f92965";
+    }
+
+    extractNowPlayingSong(jsonData:any): song.Song {
+        var artist = jsonData[0][0].artist.trim();
+        var track = jsonData[0][0].title.trim();
+
+        if (artist && track && artist != 'No Artist Info Available' && track != 'No Track info Available') {
+            return {
+                Artist: jsonData[0][0].artist.trim(),
+                Track: jsonData[0][0].title.trim()
+            };
+        } else {
+            winston.info("RockFmScraper: No song playing")
+            return { Artist: null, Track: null };
+        }
+    }
+}
+
+/*
 export class RockFmScraper extends scrap.CheerioScraper {
 	private url: string;
 
 	constructor(name:string) {
 		super(name);
-		this.url = "http://www.rockfm.co.uk";
+	    this.url = "http://www.rockfm.co.uk";
 	}
 
 	public getUrl(): string {
@@ -38,3 +69,4 @@ export class RockFmScraper extends scrap.CheerioScraper {
 		}
 	}
 }
+*/

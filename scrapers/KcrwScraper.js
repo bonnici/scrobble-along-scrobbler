8499f5aee76d1ddb41c7112c7d36ec15f0ba09a8
bonnici <nickmarsh@gmail.com>
2015-01-12 20:26:45 +1000

    Updated KCRW scraper.

--- scrapers/KcrwScraper.js
+++ scrapers/KcrwScraper.js
@@ -5,49 +5,55 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-var scrap = require("./CheerioScraper");
-
-var winston = require("winston");
+//import scrap = require("./CheerioScraper");
+var scrap = require("./JsonScraper");
 
 var KcrwScraper = (function (_super) {
     __extends(KcrwScraper, _super);
     function KcrwScraper(name) {
         _super.call(this, name);
     }
-    KcrwScraper.prototype.getUrl = function () {
-        return "http://newmedia.kcrw.com/tracklists/index.php?channel=Live";
+    KcrwScraper.prototype.getUrl = function (lastfmUsername) {
+        return "http://tracklist-api.kcrw.com/Simulcast";
     };
 
-    KcrwScraper.prototype.parseCheerio = function ($, callback) {
-        var playlistRows = $("table#table_tracklist tbody tr");
-
-        if (playlistRows.length < 1) {
-            winston.warn("KcrwScraper: Not enough playlist rows (" + playlistRows.length + ")");
-            callback(null, { Artist: null, Track: null });
-            return;
-        }
-
-        var firstSongRow = playlistRows.eq(0);
-
-        if (firstSongRow.children("td").length < 3) {
-            winston.warn("KcrwScraper: Not enough playlist cols (" + firstSongRow.children("td").length + ")");
-            callback(null, { Artist: null, Track: null });
-            return;
-        }
-
-        var artist = firstSongRow.children("td").eq(1).text();
-        var song = firstSongRow.children("td").eq(2).text();
-
-        if (!artist || artist == '' || !song || song == '' || artist == 'Break' || song == "Break") {
-            winston.warn("KcrwScraper: Invalid cols (" + artist + "/" + song + ")");
-            callback(null, { Artist: null, Track: null });
-            return;
-        }
-
-        winston.info("KcrwScraper found song " + artist + " - " + song);
-        callback(null, { Artist: artist, Track: song });
+    KcrwScraper.prototype.extractNowPlayingSong = function (jsonData) {
+        return { Artist: jsonData.artist, Track: jsonData.title };
     };
     return KcrwScraper;
-})(scrap.CheerioScraper);
+})(scrap.JsonScraper);
 exports.KcrwScraper = KcrwScraper;
+/*
+export class KcrwScraper extends scrap.CheerioScraper {
+constructor(name:string) {
+super(name);
+}
+public getUrl(): string {
+return "http://newmedia.kcrw.com/tracklists/index.php?channel=Live";
+}
+public parseCheerio($:any, callback: (err, newNowPlayingSong: song.Song, justScrobbledSong?:song.Song) => void): void {
+var playlistRows = $("table#table_tracklist tbody tr");
+if (playlistRows.length < 1) {
+winston.warn("KcrwScraper: Not enough playlist rows (" + playlistRows.length + ")");
+callback(null, { Artist: null, Track: null });
+return;
+}
+var firstSongRow = playlistRows.eq(0);
+if (firstSongRow.children("td").length < 3) {
+winston.warn("KcrwScraper: Not enough playlist cols (" + firstSongRow.children("td").length + ")");
+callback(null, { Artist: null, Track: null });
+return;
+}
+var artist = firstSongRow.children("td").eq(1).text();
+var song = firstSongRow.children("td").eq(2).text();
+if (!artist || artist == '' || !song || song == '' || artist == 'Break' || song == "Break") {
+winston.warn("KcrwScraper: Invalid cols (" + artist + "/" + song + ")");
+callback(null, { Artist: null, Track: null });
+return;
+}
+winston.info("KcrwScraper found song " + artist + " - " + song);
+callback(null, { Artist: artist, Track: song });
+}
+}
+*/
 //# sourceMappingURL=KcrwScraper.js.map
--- scrapers/KcrwScraper.ts
+++ scrapers/KcrwScraper.ts
@@ -1,10 +1,25 @@
 /// <reference path="../definitions/typescript-node-definitions/winston.d.ts"/>
 
-import scrap = require("./CheerioScraper");
+//import scrap = require("./CheerioScraper");
+import scrap = require("./JsonScraper");
 import song = require("../Song");
 
 import winston = require("winston");
 
+export class KcrwScraper extends scrap.JsonScraper {
+    constructor(name:string) {
+        super(name);
+    }
+
+    getUrl(lastfmUsername?:string): string {
+        return "http://tracklist-api.kcrw.com/Simulcast";
+    }
+
+    extractNowPlayingSong(jsonData:any): song.Song {
+        return { Artist: jsonData.artist, Track: jsonData.title };
+    }
+}
+/*
 export class KcrwScraper extends scrap.CheerioScraper {
 
 	constructor(name:string) {
@@ -45,3 +60,4 @@ export class KcrwScraper extends scrap.CheerioScraper {
 		callback(null, { Artist: artist, Track: song });
 	}
 }
+*/
